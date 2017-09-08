f00a5df9bd594a0ed1c928730bd9c9e2b92fd06a
Sergey <emavericke@yahoo.com>
2017-09-08 09:58:57 +0300

    charts

--- bitrix/templates/main/js/common.js
+++ bitrix/templates/main/js/common.js
@@ -2,6 +2,9 @@ window.onload = function() {
 	if($('video').length){
 		$('video').get(0).play();
 	}
+	setTimeout(function(){
+		$('#title-wrapper').removeClass('animated fadeInDownMenu')
+	}, 300)
 };
 
 
@@ -15,17 +18,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	var docWindow = $(window),
 		vHeight = docWindow.height(),
-		mainScreen = $('#main-screen'),
 		headerMenu = $('#header-mainwrap'),
 		videoWrapper = $('#main-video-wrapper'),
 		screenWrapperHeight = $('#main-screen-wrapper').height(),
 		burger = $('#main-burger'),
-		secMenu = $('#header-mainwrap .header-menu-modal'),
 		screenLinks = $('#main-screen .screen-links-wrapper'),
 		mainTitle = $('#title-wrapper'),
-		mobile = isMobile(),
-		screenLinksHeight = screenLinks.height(),
-		$fullPage = $('#fullpage');
+		mobile = isMobile();
 
 	// Video append if not mobile
 	if(!isMobile()){
@@ -47,16 +46,16 @@ document.addEventListener("DOMContentLoaded", function() {
 		}
 	});
 
+
 	// Main screen "three list" events
-	$('#main-screen .screen-link-text').on('mouseenter', function(){
-		if(!mainTitle.hasClass('activated')){
-			mainTitle.addClass('activated')
-		}
-		mainTitle.addClass('active')
-	});
-	$('#main-screen .screen-link-text').on('mouseleave', function(){
-		mainTitle.removeClass('active')
-	});
+	if( $('#main-screen .screen-link-text').length ){
+		$('#main-screen .screen-link-text').on('mouseenter', function(){
+			mainTitle.addClass('active')
+		});
+		$('#main-screen .screen-link-text').on('mouseleave', function(){
+			mainTitle.removeClass('active')
+		});
+	}
 
 
 	// Main screen fade out and video zoom out on scroll
@@ -84,11 +83,10 @@ document.addEventListener("DOMContentLoaded", function() {
 	// PARALLAX event on scroll trigger
 	if($('#parallax').length){
 		var $parallax = $('#parallax'),
-			$parallaxImg = $('#parallax>img');
-
-		parallaxFieldViewTop = $parallax.offset().top - vHeight
-		parallaxFieldViewBottom = $parallax.offset().top + $parallax.height()
-		parallaxFieldView = parallaxFieldViewBottom - parallaxFieldViewTop
+			$parallaxImg = $('#parallax .parallax-img img'),
+			parallaxFieldViewTop = $parallax.offset().top - vHeight,
+			parallaxFieldViewBottom = $parallax.offset().top + $parallax.height(),
+			parallaxFieldView = parallaxFieldViewBottom - parallaxFieldViewTop;
 
 		function scrollParallax(scrollPos){
 			console.log()
@@ -103,6 +101,44 @@ document.addEventListener("DOMContentLoaded", function() {
 	// 	console.log('Height '+docFunctions.findHeight())
 	// });
 
+	if($('#dates').length){
+
+
+		$('#dates-slider').slick({
+			slidesToShow: 4,
+			slidesToScroll: 4,
+			infinite: false,
+			responsive: [
+				{
+					breakpoint: 1024,
+					settings: {
+						slidesToShow: 3,
+						slidesToScroll: 3,
+						arrows: false,
+						infinite: false
+					}
+				},
+				{
+					breakpoint: 750,
+					settings: {
+						slidesToShow: 2,
+						slidesToScroll: 2,
+						arrows: false,
+						infinite: false
+					}
+				},
+				{
+					breakpoint: 540,
+					settings: {
+						slidesToShow: 1,
+						slidesToScroll: 1,
+						arrows: false,
+						infinite: false
+					}
+				}
+			]
+		});
+	}
 
 	// Scroll triggers
 	docWindow.scroll(function (event) {
@@ -123,10 +159,10 @@ document.addEventListener("DOMContentLoaded", function() {
 		}
 
 
-    	if(!mobile){
-    		scrollMainScreen(scrollPos);
-    		scrollMainTitle(scrollPos);
-    	}
+		if(!mobile){
+			scrollMainScreen(scrollPos);
+			scrollMainTitle(scrollPos);
+		}
 
 		headerMenu.removeClass('sticked animated fadeInDownFast')
 
@@ -136,8 +172,8 @@ document.addEventListener("DOMContentLoaded", function() {
 	if($(window).width() >= 1024){
 		inView.offset(100)
 		inView('.animateThis').on('enter', function(el){
-	    	$(el).addClass('animated ' + $(el).data('anim'))
-	    });
+			$(el).addClass('animated ' + $(el).data('anim'))
+		});
 	}
 
 	// Chart bar width maker
@@ -148,240 +184,159 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 	// Brands appender and changer
-	var elemsInView = [],
-		isPaused = false,
-		$brands = $('#brands')
-	$brandsBlocks = $('#brands .brand-anim-block');
+	if($('#brands').length){
+		var elemsInView = [],
+			isPaused = false,
+			$brands = $('#brands'),
+			$brandsBlocks = $('#brands .brand-anim-block');
 
-	$brandsBlocks.filter(function(el){
-		var _this = $(this),
-			rand = Math.floor(Math.random() * (15000 - 8000)) + 8000,
-			brandCounter = 0;
+		$brandsBlocks.filter(function(el){
+			var _this = $(this),
+				rand = Math.floor(Math.random() * (15000 - 8000)) + 8000,
+				brandCounter = 0;
 
-		// Brand appender
-		if(brandCounter < $brandsBlocks.length+1){
+			// Brand appender
+			if(brandCounter < $brandsBlocks.length+1){
 
-			// Random element
-			var elem = Math.floor(Math.random() * brandElems.brands.length)
+				// Random element
+				var elem = Math.floor(Math.random() * brandElems.brands.length)
 
-			// Random while element exist
-			while(elemsInView[elem]){
-				elem = Math.floor(Math.random() * brandElems.brands.length)
+				// Random while element exist
+				while(elemsInView[elem]){
+					elem = Math.floor(Math.random() * brandElems.brands.length)
+				}
+
+				// Write element in array
+				elemsInView[elem] = true;
+
+				// Append brand
+				_this.append('<a href="' + brandElems.brands[elem].href + '"><img src="' + brandElems.brands[elem].src + '" alt="brand"></a>')
+
+				brandCounter++;
 			}
 
-			// Write element in array
-			elemsInView[elem] = true;
+			// Brands changer
+			if(!mobile){
+				setInterval( function(){
 
-			// Append brand
-			_this.append('<a href="' + brandElems.brands[elem].href + '"><img src="' + brandElems.brands[elem].src + '" alt="brand"></a>')
+					// Skip changing when mouse enter
+					if(!isPaused){
 
-			brandCounter++;
-		}
-
-		// Brands changer
-		if(!mobile){
-			setInterval( function(){
-
-				// Skip changing when mouse enter
-				if(!isPaused){
-
-					// Random while elem exist
-					while(elemsInView[elem]){
-						elem = Math.floor(Math.random() * brandElems.brands.length);
-					}
-
-					// Append new brand
-					_this.append('<a href="' + brandElems.brands[elem].href + '"><img src="' + brandElems.brands[elem].src + '" alt="brand"></a>');
-
-					// Animating new appended brand
-					_this.children('a:nth-of-type(2)').addClass('animated slideBrand')
-
-					// Animate Out old Brand
-					_this.children('a:first-of-type').addClass('animated slideBrandOut')
-
-					// Write new brand
-					elemsInView[elem] = true;
-
-					// Removing old brand after 1 sec
-					setTimeout(function(){
-
-						// Search and destroy old brand
-						for(var key in brandElems.brands){
-							if(brandElems.brands[key].src == _this.children('a:first-of-type').children('img').attr('src')){
-								elemsInView[key] = false;
-							}
+						// Random while elem exist
+						while(elemsInView[elem]){
+							elem = Math.floor(Math.random() * brandElems.brands.length);
 						}
 
-						// Remove animation from new brand
-						_this.children('a').removeClass('animated slideBrand');
+						// Append new brand
+						_this.append('<a href="' + brandElems.brands[elem].href + '"><img src="' + brandElems.brands[elem].src + '" alt="brand"></a>');
 
-						// Destroy old brand
-						_this.children('a:first-of-type').remove();
+						// Animating new appended brand
+						_this.children('a:nth-of-type(2)').addClass('animated slideBrand')
 
-					}, 1000);
+						// Animate Out old Brand
+						_this.children('a:first-of-type').addClass('animated slideBrandOut')
 
-				}
-			}, rand)
-		}
-	})
+						// Write new brand
+						elemsInView[elem] = true;
 
-	$brands.on('mouseenter', function(){
-		isPaused = true
-	})
-	$brands.on('mouseleave', function(){
-		isPaused = false
-	})
+						// Removing old brand after 1 sec
+						setTimeout(function(){
 
+							// Search and destroy old brand
+							for(var key in brandElems.brands){
+								if(brandElems.brands[key].src == _this.children('a:first-of-type').children('img').attr('src')){
+									elemsInView[key] = false;
+								}
+							}
+
+							// Remove animation from new brand
+							_this.children('a').removeClass('animated slideBrand');
+
+							// Destroy old brand
+							_this.children('a:first-of-type').remove();
+
+						}, 1000);
+
+					}
+				}, rand)
+			}
+		})
+
+		$brands.on('mouseenter', function(){
+			isPaused = true
+		})
+		$brands.on('mouseleave', function(){
+			isPaused = false
+		})
+
+	}
 
 	// Scheme rotator and activator
-	var ell = 1,
-		$schemeButton = $('#scheme .scheme-button'),
-		$schemeSlider = $('#scheme-slider'),
-		$schemeSlides = $('#scheme-slider .scheme-slider__slide');
+	if($('#scheme').length){
+		var ell = 1,
+			$schemeButton = $('#scheme .scheme-button'),
+			$schemeSlides = $('#scheme-slider .scheme-slider__slide');
 
-	$schemeButton.on('click', function(){
-		var _self = $(this);
-		console.log(ell)
-		if(!_self.hasClass('active')){
+		$schemeButton.on('click', function(){
+			var _self = $(this);
+			console.log(ell)
+			if(!_self.hasClass('active')){
 
-			// Removing all active classes on buttons
-			$schemeButton.filter(function(){
-				$(this).removeClass('active')
-			});
+				// Removing all active classes on buttons
+				$schemeButton.filter(function(){
+					$(this).removeClass('active')
+				});
 
-			// Removing all active classes on slides
-			// $schemeSlides.eq(ell-1).removeClass('active')
-
-
-			// Add active class to pressed button
-			_self.addClass('active').siblings().removeClass('active');
+				// Removing all active classes on slides
+				// $schemeSlides.eq(ell-1).removeClass('active')
 
 
-			var slide = $schemeSlides.eq(_self.data('slide')-1);
-
-			slide.addClass('active').siblings().removeClass('active');
-
-			ell = _self.data('slide')
-			$('#scheme .scheme-menu').css('transform', 'rotate(' + (((360/8)*(- _self.data('slide'))) + 45) + 'deg)')
-			$('#scheme .scheme-menu g.icon').css('transform', 'rotate(' + (((360/8)*(_self.data('slide')))-45) + 'deg)')
-		}
-	})
-
-	// Scheme update button trigger
-	$('#update-scheme-slider').on('click', function(){
-		ell >= 8 ? ell = 1 : ell++;
-		$schemeButton.eq(ell-1).trigger('click')
-		console.log(ell)
-	})
+				// Add active class to pressed button
+				_self.addClass('active').siblings().removeClass('active');
 
 
-	if($('.left_scroll-container').length){
+				var slide = $schemeSlides.eq(_self.data('slide')-1);
 
+				slide.addClass('active').siblings().removeClass('active');
 
-		var scrolling = false,
-        	contentSections = $('.block-section'),
-			verticalNavigation = $('.left_scroll_pagin-wrap'),
-			navigationItems = verticalNavigation.find('.left_scroll-item');
-
-		$(window).on('scroll', checkScroll);
-
-		function checkScroll() {
-			if( !scrolling ) {
-				scrolling = true;
-				(!window.requestAnimationFrame) ? setTimeout(updateSections, 300) : window.requestAnimationFrame(updateSections);
+				ell = _self.data('slide')
+				$('#scheme .scheme-menu').css('transform', 'rotate(' + (((360/8)*(- _self.data('slide'))) + 45) + 'deg)')
+				$('#scheme .scheme-menu g.icon').css('transform', 'rotate(' + (((360/8)*(_self.data('slide')))-45) + 'deg)')
 			}
-		}
+		})
 
-		verticalNavigation.on('click','.left_scroll-item',function(event){
-			event.preventDefault();
-			smoothScroll($(this));
-			verticalNavigation.removeClass('open');
+		// Scheme update button trigger
+		$('#update-scheme-slider').on('click', function(){
+			ell >= 8 ? ell = 1 : ell++;
+			$schemeButton.eq(ell-1).trigger('click');
+			console.log(ell);
+		})
+	}
+
+	if($('#graphics').length){
+		MG.data_graphic({
+			//title: "Производство мяса птицы, тыс. тонн",
+			data: chartData[0].values,
+			animate_on_load: true,
+			width: 600,
+			height: 500,
+			left: 100,
+			y_extended_ticks: true,
+			x_extended_ticks: true,
+			target: '#company-chart',
+			x_accessor: 'date',
+			y_accessor: 'value',
+			y_label: chartData[0].title,
+			color_accessor: 'v',
+        	color_type:'category',
+        	mouseover: function(d, i) {
+            // custom format the rollover text, show days
+		var pf = d3.format('.0s');
+		d3.select('#company-chart svg .mg-active-datapoint')
+                .text('В ' + (i + 2011) + ' году произведено ' + d.value+' тыс.тонн');
+        	},
 		});
-
-		function updateSections() {
-			var halfWindowHeight = $(window).height()/2,
-				scrollTop = $(window).scrollTop();
-			contentSections.each(function(){
-				var section = $(this),
-					sectionId = section.attr('id'),
-					count = section.data('count'),
-					trg = section.data('target');
-				navigationItem = navigationItems.filter("[data-target=" + trg + "]");
-				( (section.offset().top - halfWindowHeight < scrollTop ) && ( section.offset().top + section.outerHeight() - halfWindowHeight > scrollTop) )
-                        ? navigationItem.addClass('active')
-                        : navigationItem.removeClass('active');
-			});
-			scrolling = false;
-		}
-
-		function smoothScroll(target) {
-			var data = target.data('target');
-			var elem = $('.' + data);
-
-			$('body,html').animate(
-            {'scrollTop':elem.offset().top},
-            500
-        );
-		}
-
 	}
 
 
 })
-
-
-
-/*var ctx = document.getElementById("index-chart").getContext('2d');
-	var myChart = new Chart(ctx, {
-	    type: 'horizontalBar',
-	    data: {
-	        labels: ["Корма и компоненты", "Мясо птицы", "Мясо-колбасные изделия", "Молоко"],
-	        datasets: [{
-	            label: '# of Votes',
-	            data: [100, 90, 80, 70],
-	            backgroundColor: [
-	                'rgba(86, 182, 34, 1)',
-	                'rgba(86, 182, 34, 0.8)',
-	                'rgba(86, 182, 34, 0.6)',
-	                'rgba(86, 182, 34, 0.4)'
-	            ],
-	            hoverBackgroundColor: '#0056b8'
-	        }]
-	    },
-	    options: {
-	    	layout: {
-	            padding: {
-	                left: 0,
-	                right: 0,
-	                top: 0,
-	                bottom: 0
-	            }
-	        },
-	        legend: {
-	        	display: false
-	        },
-	        scales: {
-	        	pointLabels :{
-		           display: false
-		        },
-	            yAxes: [{
-	                ticks: {
-	                    beginAtZero:true
-	                },
-	                gridLines: {
-                    	display: false
-                	},
-                	categoryPercentage: 1.0,
-           			barPercentage: 1.0
-	            }],
-	            xAxes: [{
-		            stacked: true,
-		            gridLines: {
-	                   	display: false
-	                },
-	                categoryPercentage: 1.0,
-            		barPercentage: 1.0
-			    }]
-	        },
-	    }
-	});*/
--- src/js/lib.js
+++ src/js/lib.js
@@ -4,4 +4,3 @@
 //=include lib/in-view.min.js
 
 
-
